c8788a63b7599b02ba606020aea1e3798d67f110
OmarElmehi770 <omar770949@gmail.com>
2025-07-25 16:39:10 +0300

    new

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatefulWidget {
   @override
   State<MyApp> createState() => _MyAppState();
 }
-// hallo
+
 class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
