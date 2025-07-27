6763aa97e1c72228efc045f24595ed431616349d
OmarElmehi770 <omar770949@gmail.com>
2025-07-27 05:33:23 +0300

    new

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -7,7 +7,6 @@ import '../rooms&halls/views/halls.dart';
 import '../rooms&halls/views/rooms.dart';
 import 'Widgets1/custom_container.dart';
 import 'Widgets1/custom_device_container.dart';
-import 'Widgets1/drawer.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -26,7 +25,119 @@ class _HomeState extends State<Home> {
         title: Text("Welcome Home", style: TextStyle(fontSize: 16)),
         actions: [Image.asset('assets/images/IEEE.png')],
       ),
-      drawer: CustomDrawer(),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 60,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.4),
+                      spreadRadius: 2,
+                      blurRadius: 6,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                width: double.infinity,
+                height: 50,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      child: Text(
+                        'Security',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          x = !x;
+                        });
+                      },
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 300),
+                        width: 100,
+                        height: 50,
+                        child: Icon(
+                          x ? Icons.toggle_on : Icons.toggle_off,
+                          size: 50,
+                          color: x ? const Color(0xFF0207FD) : Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.4),
+                      spreadRadius: 2,
+                      blurRadius: 6,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                width: double.infinity,
+                height: 50,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      child: Text(
+                        'Power Saving',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          y = !y;
+                        });
+                      },
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 300),
+                        width: 100,
+                        height: 50,
+                        child: Icon(
+                          y ? Icons.toggle_on : Icons.toggle_off,
+                          size: 50,
+                          color: y ? const Color(0xFF0207FD) : Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Center(
